623ef76376bc09a0844ab5a51c4a7c254c0b65d0
marcus_mattivi <marcus_mattivi@estudante.sesisenai.org.br>
2023-03-22 12:21:26 -0300

    Inicial

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
     }
-    import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.Toast;
 
     public class Comon extends AppCompatActivity {
         EditText PN, SN;
@@ -25,44 +25,29 @@ import android.widget.Toast;
             PN = findViewById(R.id.PN); //Vincula um atributo java com o do xml
             PN = findViewById(R.id.PN);
 
-            public void subtrai Object View;
+            public void subtraçao Object View;
             (View v){
-                while(PN>0){
-                    PN --;
-                    PN +-= 60;
+                while (PN > 0) {
+                    PN--;
+                    PN + -= 60;
                 }
-                while(SN>0){
-                    SN --;
+                while (SN > 0) {
+                    SN--;
                     SN += 60;
                 }
-                int resultHora = 0;
-                int resultMin = PN-SN;
-                if(PN>SN){
-                    resultMin = PN-SN;
+                int resultCalculo = PN - SN;
+                int resultCalculo = PN + SN;
                 }
-                while(resultMin = 59){
-                    resultHora ++;
-                    resultMin -= 60;
-                }
-                RC.setText(resultCalculo+"");
+                RC.setText(resultCalculo + "PN,SN");
 
                 @Override
-                protected void onCreate(Bundle savedInstanceState) {
+                protected void onCreate (Bundle savedInstanceState){
                     super.onCreate(savedInstanceState);
                     setContentView(R.layout.activity_main);
                 }
-                public void calculoSoma(View v){
-                    int HoI = Integer.parseInt(PN.getText().toString());
-                    int MinI = Integer.parseInt(PN.getText().toString());
-                    int HoF = Integer.parseInt(SN.getText().toString());
-                    int MinF = Integer.parseInt(SN.getText().toString());
-                    int HoraFinal = HoI+HoF;
-                    int MinFinal = MinI+MinF;
-                    String Mensagem = "Os valores são invalidos"; //String para valores negativos
-                    while (MinFinal>=60){
-                        MinFinal = MinFinal-60;
-                        HoraFinal = HoraFinal+1;
-                    }
-                    RC.setText(PrimeiroNumero+":"+SegundoNumero);
+                public void calculoSoma (View v){
+                    int PrN = Integer.parseInt(PN.getText().toString());
+                    int SgN = Integer.parseInt(SN.getText().toString());
+                    int resultCalculo = PN + SN;
                 }
-}
+            }
