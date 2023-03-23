f9502edf4686079607840ceac556614a5477cbde
marcus_mattivi <marcus_mattivi@estudante.sesisenai.org.br>
2023-03-23 09:37:14 -0300

    Inicial

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,50 +1,49 @@
 package com.example.calculadora;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import java.util.Scanner;
-
 public class MainActivity extends AppCompatActivity {
     EditText num1, num2;
+    TextView resultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         num1 = findViewById(R.id.num1);
         num2 = findViewById(R.id.num2);
+        resultado = findViewById(R.id.resultado);
         int resultado = 0;
         float resultFloat = 0;
     }
-    public static class Calc {
-        /*Declaração dos métodos*/
-        public int som(int num1, int num2) {
-            return num1 + num2;
-        }
 
-        public int sub(int num1, int num2) {
-            return num1 - num2;
-        }
+    public void calculoSom(View v) {
+        int num1 = Integer.parseInt(num1.getText().toString());
+        int num2 = Integer.parseInt(num2.getText().toString());
+        resultado.setText(num1 + num2);
+    }
 
-        public int div(int num1, int num2) {
-            return num1 / num2;
-        }
+    public void calculoSub(View v) {
+        int num1 = Integer.parseInt(num1.getText().toString());
+        int num2 = Integer.parseInt(num2.getText().toString());
+        resultado.setText(num1 - num2);
+    }
 
-        public int mult(int num1, int num2) {
-            return num1 * num2;
-        }
+    public void calculoDiv(View v) {
+        int num1 = Integer.parseInt(num1.getText().toString());
+        int num2 = Integer.parseInt(num2.getText().toString());
+        resultado.setText(num1 / num2);
+    }
 
-        public void main(String[] args) {
-            //criando um objeto c a apartir do metodo calc
-            Calc c = new Calc();
-
-            //declarando as varíaveis
-            int num1;
-            int num2;
-
-                }
-            }
-        }
+    public void calculoMult(View v) {
+        int num1 = Integer.parseInt(num1.getText().toString());
+        int num2 = Integer.parseInt(num2.getText().toString());
+        resultado.setText(num1 * num2);
+    }
+}
