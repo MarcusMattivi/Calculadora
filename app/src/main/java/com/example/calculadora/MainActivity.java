732a9f6b56a95a88524328440d46eb0f3fef7944
marcus_mattivi <marcus_mattivi@estudante.sesisenai.org.br>
2023-03-23 08:31:03 -0300

    Inicial

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.calculadora;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText num1, num2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        int result = 0;
+        float resultFloat = 0;
+    }
     public static class Calc {
         /*Declaração dos métodos*/
         public int som(int num1, int num2) {
