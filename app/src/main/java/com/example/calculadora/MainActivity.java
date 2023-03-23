3924d4a1b27042ac5a3c09debec1bede84a1c2d4
marcus_mattivi <marcus_mattivi@estudante.sesisenai.org.br>
2023-03-23 07:49:53 -0300

    Inicial

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -11,27 +11,21 @@ public class MainActivity extends AppCompatActivity {
     public class Calc {
         /*Declaração dos métodos*/
         public int som(int num1, int num2) {
-
             return num1 + num2;
-
         }
         public int sub(int num1, int num2){
-
             return num1 - num2;
-
         }
         public int div(int num1,int num2){
-
             return num1 / num2;
-
         }
         public int mult(int num1, int num2){
-
-            return num1 - num2;
-
+            return num1 * num2;
         }
-        public static void main (String args[]){
 
+        // --------------- Metodo principal
+
+        public static void main (String args[]){
             //criando um objeto c a apartir do metodo calc
             Calc c = new Calc();
 
@@ -41,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
             int num2;
 
             Scanner input = new Scanner(System.in);
-
-
             System.out.println("-Escolha uma opção-");
             System.out.println("1. Soma");
             System.out.println("2. Subtracao");
@@ -54,66 +46,37 @@ public class MainActivity extends AppCompatActivity {
             opcao = input.nextInt();
 
             while (opcao != 0) {
+                Scanner input1 = new Scanner(System.in);
 
+                System.out.println("Qual o primeiro numero: ");
+                num1 = input1.nextInt();
+                System.out.println("Qual o segundo numero: ");
+                num2 = input1.nextInt();
 
-                if (opcao == 1) {
-
-
-                    Scanner input1 = new Scanner(System.in);
-
-                    System.out.println("Qual o primeiro numero: ");
-                    num1 = input1.nextInt();
-                    System.out.println("Qual o segundo numero: ");
-                    num2 = input1.nextInt();
-
+                if ( opcao == 1 ) {
                     int operacao = c.som(num1, num2);
-
-                    System.out.println(operacao);
+                    System.out.printf("\nO resultado da soma é: %d\n", operacao);
                     break;
                 }
-                if (opcao == 2) {
-
-                    Scanner input1 = new Scanner(System.in);
-
-                    System.out.println("Qual o primeiro numero: ");
-                    num1 = input1.nextInt();
-                    System.out.println("Qual o segundo numero: ");
-                    num2 = input1.nextInt();
+                else if (opcao == 2) {
                     int operacao = c.sub(num1, num2);
-
-                    System.out.println(operacao);
-
+                    System.out.printf("\nO resultado da subtração é: %d\n", operacao);
                     break;
                 }
-                if (opcao == 3) {
-                    Scanner input1 = new Scanner(System.in);
-
-                    System.out.println("Qual o primeiro numero: ");
-                    num1 = input1.nextInt();
-                    System.out.println("Qual o segundo numero: ");
-                    num2 = input1.nextInt();
+                else if (opcao == 3) {
                     int operacao = c.mult(num1, num2);
-
-                    System.out.println(operacao);
+                    System.out.printf("\nO resultado da multiplicação é: %d\n", operacao);
                     break;
                 }
-                if (opcao == 4) {
-                    Scanner input1 = new Scanner(System.in);
-
-                    System.out.println("Qual o primeiro numero: ");
-                    num1 = input1.nextInt();
-                    System.out.println("Qual o segundo numero: ");
-                    num2 = input1.nextInt();
+                else if (opcao == 4) {
                     int operacao = c.div(num1, num2);
-
-                    System.out.println(operacao);
+                    System.out.printf("\nO resultado da divisão é: %d\n", operacao);
                     break;
                 }
                 else{
                     System.out.println("????");
                     break;
                 }
-
-            }
-        }
+            } // fim do while - usuario optou por sair
+        }  // fim do metodo principal
     }
